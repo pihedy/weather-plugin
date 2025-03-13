a94c135638dced851d1235c7c78e3ea4991e7230
pihedy <pihedy@gmail.com>
2025-03-13 19:06:17 +0100

    Init base files.

---
+++ weather-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Weather Plugin (Centrál Mediacsoport Zrt. feladat)
+ * Plugin URI:  https://github.com/pihedy/weather-plugin
+ * Description: Ez a plugin az OpenWeatherMap API segítségével mutatja az aktuális időjárást.
+ * Version:     1.0.0
+ * Author:      Pihe Edmond
+ * Author URI:  https://pihedy.github.io/
+ * Text Domain: wpcm
+ */
+
+declare(strict_types=1);
