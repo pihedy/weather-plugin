50e4fc1dc81d34f0be109f780235c92b4b1feb24
pihedy <pihedy@gmail.com>
2025-03-13 19:33:38 +0100

    Add base autoload.

--- weather-plugin.php
+++ weather-plugin.php
@@ -10,3 +10,52 @@
  */
 
 declare(strict_types=1);
+
+/**
+ * Recursively scans a directory for PHP files and populates the files array.
+ *
+ * @param string $dir The directory path to scan.
+ * @param array  &$files Reference to an array that will be populated with PHP file paths.
+ */
+function scanFiles(string $dir, array &$files): void
+{
+    $iterator = new DirectoryIterator($dir);
+
+    foreach ($iterator as $info) {
+        if ($info->isDot()) {
+            continue;
+        }
+
+        if ($info->isDir()) {
+            scanFiles($info->getPathname(), $files);
+
+            continue;
+        }
+
+        if (strtolower($info->getExtension()) === 'php') {
+            $files[] = $info->getPathname();
+        }
+    }
+}
+
+/**
+ * Autoloads files based on a provided identifier and file path.
+ */
+$Require = \Closure::bind(static function (mixed $identifier, string $file): void {
+    if (!empty($GLOBALS['_wpcm_autoload_files'][$identifier])) {
+        return;
+    }
+
+    $GLOBALS['_wpcm_autoload_files'][$identifier] = true;
+
+    require __DIR__ . $file;
+}, null, null);
+
+$srcDir = __DIR__ . '/src';
+$files = [];
+
+scanFiles($srcDir, $files);
+
+foreach ($files as $index => $file) {
+    $Require($index, $file);
+}
